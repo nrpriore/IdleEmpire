e3195a9a07871ae39868d0330d90326e7d4d0da6
nrpriore <nr.priore@gmail.com>
2019-06-10 09:18:28 -0400

    wip

---
+++ Assets/Scripts/Util/GameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData {
+
+}
+
+
+// Static Tile data
+public class TileData {
+	public GridType Type {get;set;}
+	public int ID {get;set;}
+	public string Name {get;set;}
+	
+}
+
+// Enum for tabs
+public enum GridType {
+	Econ = 0,
+	War = 1,
+	Sci = 2
+}
---
+++ Assets/Scripts/Util/Prefabs.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+// Holds references to all spawnable prefabs
+public class Prefabs {
+	public GameObject[] Econ;
+	public GameObject[] Sci;
+	public GameObject[] War;
+}
